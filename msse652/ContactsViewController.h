3d6eeff3c3e0ff6845427365bc43da365bc74f70
echolush <echolush@echolush-4.local>
2014-07-02 11:35:45 -0700

    Week 1
    - Added files for view and table view controllers

---
+++ msse652/ContactsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ContactsViewController.h
+//  msse652
+//
+//  Created by echolush on 7/2/14.
+//  Copyright (c) 2014 Matt Ozer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContactsViewController : UIViewController
+
+@end
---
+++ msse652/TasksViewController.h
@@ -0,0 +1,13 @@
+//
+//  TasksViewController.h
+//  msse652
+//
+//  Created by echolush on 7/2/14.
+//  Copyright (c) 2014 Matt Ozer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TasksViewController : UIViewController
+
+@end
